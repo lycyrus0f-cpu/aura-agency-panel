631d62fffd12859dc038cfa25dd48f0a5b14b78f
lycyrus0f-cpu <ly.cyrus0f@gmail.com>
2026-08-06 02:56:26 +0400

    Add Firebase web configuration

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.AURA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAN0vErEN2uXfngSI4I3i0ajhpAIkTv3ys",
+  authDomain: "aura-agency-panel.firebaseapp.com",
+  projectId: "aura-agency-panel",
+  storageBucket: "aura-agency-panel.firebasestorage.app",
+  messagingSenderId: "511207409197",
+  appId: "1:511207409197:web:5580c39335511b02e95aa4",
+  measurementId: "G-BERS574BFK"
+};
